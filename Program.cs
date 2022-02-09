4ff95402440ba510e6c572eb456a0206eb875ec0
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-09 16:45:10 -0600

    uso de while en c#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UsoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            int intentos = 0, nRandom, userNum;
+
+            Random n = new Random();
+            nRandom = n.Next(0, 100);
+            Console.WriteLine("ingresa el numero");
+            userNum = int.Parse(Console.ReadLine());
+            while (nRandom != userNum)
+            {
+                intentos++;
+                userNum = int.Parse((Console.ReadLine()));
+                if (nRandom == userNum) Console.WriteLine("FELICIDADES");
+                if (intentos == 2)
+                {
+                    Console.WriteLine($"PERDISTE \nnumero: {nRandom}");
+                    break;
+                }
+            }
+        }
+    }
+}
